5d3d7d339c3cf0df30d23de517e945989506b1c9
khalfan7 <khalfan.h@outlook.com>
2020-05-04 04:16:20 +0400

    AP_HAL/AP_CHIBIOS

--- libraries/AP_HAL/HAL.h
+++ libraries/AP_HAL/HAL.h
@@ -1,5 +1,7 @@
 #pragma once
 
+class AP_Param;
+
 #include "AP_HAL_Namespace.h"
 
 #include "AnalogIn.h"
@@ -15,6 +17,7 @@
 #include "CAN.h"
 #endif
 
+
 class AP_HAL::HAL {
 public:
     HAL(AP_HAL::UARTDriver* _uartA, // console
@@ -23,6 +26,8 @@ public:
         AP_HAL::UARTDriver* _uartD, // telem2
         AP_HAL::UARTDriver* _uartE, // 2nd GPS
         AP_HAL::UARTDriver* _uartF, // extra1
+        AP_HAL::UARTDriver* _uartG, // extra2
+        AP_HAL::UARTDriver* _uartH, // extra3
         AP_HAL::I2CDeviceManager* _i2c_mgr,
         AP_HAL::SPIDeviceManager* _spi,
         AP_HAL::AnalogIn*   _analogin,
@@ -34,6 +39,7 @@ public:
         AP_HAL::Scheduler*  _scheduler,
         AP_HAL::Util*       _util,
         AP_HAL::OpticalFlow *_opticalflow,
+        AP_HAL::Flash *_flash,
 #if HAL_WITH_UAVCAN
         AP_HAL::CANManager* _can_mgr[MAX_NUMBER_OF_CAN_DRIVERS])
 #else
@@ -46,6 +52,8 @@ public:
         uartD(_uartD),
         uartE(_uartE),
         uartF(_uartF),
+        uartG(_uartG),
+        uartH(_uartH),
         i2c_mgr(_i2c_mgr),
         spi(_spi),
         analogin(_analogin),
@@ -56,7 +64,8 @@ public:
         rcout(_rcout),
         scheduler(_scheduler),
         util(_util),
-        opticalflow(_opticalflow)
+        opticalflow(_opticalflow),
+        flash(_flash)
     {
 #if HAL_WITH_UAVCAN
         if (_can_mgr == nullptr) {
@@ -95,6 +104,8 @@ public:
     AP_HAL::UARTDriver* uartD;
     AP_HAL::UARTDriver* uartE;
     AP_HAL::UARTDriver* uartF;
+    AP_HAL::UARTDriver* uartG;
+    AP_HAL::UARTDriver* uartH;
     AP_HAL::I2CDeviceManager* i2c_mgr;
     AP_HAL::SPIDeviceManager* spi;
     AP_HAL::AnalogIn*   analogin;
@@ -106,6 +117,7 @@ public:
     AP_HAL::Scheduler*  scheduler;
     AP_HAL::Util        *util;
     AP_HAL::OpticalFlow *opticalflow;
+    AP_HAL::Flash       *flash;
 #if HAL_WITH_UAVCAN
     AP_HAL::CANManager* can_mgr[MAX_NUMBER_OF_CAN_DRIVERS];
 #else
